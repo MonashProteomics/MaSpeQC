a2d7298ef18c5783daf156825e87f6784b3a4c39
CodeCaven <simon.j.caven@gmail.com>
2025-07-07 16:03:51 +1000

    Process script mod, new server script for Windows

--- mpmf-server/process.bat
+++ mpmf-server/process.bat
@@ -2,9 +2,16 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET me=%~n0
 
+:: activate python environment
+cd ..\mpmf-pipeline\.venv\Scripts
 CALL activate.bat
-cd ..\mpmf-pipeline
+
+:: run processing
+cd ..\..
 python "MPMF_Process_Raw_Files.py" %1 %2 %3
+
+:: deactivate python environment
+CALL .venv\Scripts\deactivate.bat
 
 @ECHO ON
 @EXIT /B 0
