dc4dd50967e04cb290044988ee16d11456e81004
CodeCaven <simon.j.caven@gmail.com>
2025-07-09 17:17:00 +1000

    Removing mysql script

--- mpmf-pipeline/startMYSQL.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Running MaSpeQC database server. Leave this Window open while using MaSpeQC.
-echo To run the database as a service see 'https://dev.mysql.com/doc/refman/8.4/en/windows-start-service.html'
-cd ..\Software\mysql-5.7.41-winx64/bin
-mysqld
